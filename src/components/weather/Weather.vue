f72325bf2995c0869a6a6890ada2f7bf3fd3bdef
aleksei-isakov <second-to-none18@gmail.com>
2023-01-23 13:25:23 +0300

    Weather data reactivity added

--- src/components/weather/Weather.vue
+++ src/components/weather/Weather.vue
@@ -5,14 +5,19 @@
       <div class="weather-info">
         <div class="weather-item location-selector">
           <label>
-            <input @input="getCity()" v-model="search">
+            <input @input="SearchCity" v-model="search">
           </label>
           <ul>
             <li v-for="city in foundCities" @click="selectCity(city)">{{city.name}} {{city
                 .country}}</li>
           </ul>
         </div>
-        <div class="weather-item main-weather">5</div>
+        <div class="weather-item main-weather">
+          <p>{{weather.data.temperature}}</p>
+          <p>{{weather.data.feels_like}}</p>
+          <p>{{weather.data.pressure}}</p>
+          <p>{{weather.data.humidity}}</p>
+        </div>
         <div class="weather-item temperature">2</div>
         <div class="weather-item humidity">5</div>
         <div class="weather-item wind">5</div>
@@ -25,32 +30,68 @@
 
 <script setup lang="ts">
 import axios from "axios";
-import { ref, onMounted, reactive } from 'vue'
+import { ref, reactive, watch, computed, onMounted } from 'vue'
 
+const key = '08e6a6a1566274461f6738badb1537fc'
 let search = ref('')
 let foundCities = reactive([])
-let currentCity = reactive([])
+
+let currentCityCoordinates = reactive({
+  lat: '',
+  lon: ''
+})
+
+let currentCityWeather = reactive({
+  main: {},
+  weather: {},
+  wind: {}
+})
+
+let weather = reactive( {
+  data: computed( {
+    get() {
+      return {
+        temperature: `Temperature: ${(currentCityWeather.main.temp - 273.15).toFixed(1)} °C`,
+        feels_like: `Feels like ${(currentCityWeather.main.feels_like - 273.15).toFixed(1)} °C`,
+        humidity: `Humidity: ${currentCityWeather.main.humidity} %`,
+        pressure: `Pressure: ${currentCityWeather.main.pressure} hPa`,
+        wind: `Wind speed ${currentCityWeather.wind.speed} m/s`,
+        weather: currentCityWeather.weather.main,
+        weather_icon: currentCityWeather.weather.icon
+      }
+    },
+    set() {
+    }
+  })
+})
+
+
+watch( currentCityCoordinates, async () => {
+  let responseData = await
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${currentCityCoordinates.lat}&lon=${currentCityCoordinates.lon}&appid=${key}`)
+  currentCityWeather.main = responseData.data.main;
+  currentCityWeather.weather = responseData.data.weather[0];
+  currentCityWeather.wind = responseData.data.wind
+})
 
 function selectCity(city) {
-  currentCity = city
+  currentCityCoordinates.lat = city.lat
+  currentCityCoordinates.lon = city.lon
   foundCities = []
 }
 
-async function getCity() {
-  let key = '08e6a6a1566274461f6738badb1537fc'
+async function SearchCity() {
   if (search.value) {
     let searchResults =
     await axios.get
     (`http://api.openweathermap.org/geo/1.0/direct?q=${search.value}&limit=5&appid=${key}`)
     foundCities = searchResults.data
   }
-  else
-    foundCities = []
-  // let lat = city.data[0].lat
-  // let lon = city.data[0].lon
-  // let response = await
-  //     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
+
 }
+onMounted(() => {
+  selectCity({lat: '59.9387', lon: '30.3162'})
+})
 </script>
 
 <style scoped lang="scss">
